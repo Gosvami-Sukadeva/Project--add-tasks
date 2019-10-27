a95a605fb69e9d8d10c0b1c55d08cbb22196d3ea
Mateusz Pilarczyk <bhakta666@gmail.com>
2019-10-27 12:52:37 +0100

    add search input and javascript code

--- main.js
+++ main.js
@@ -6,6 +6,22 @@ const ul = document.querySelector('ul');
 const taskNumber = document.querySelector('h1 span');
 const listItems = document.getElementsByClassName('task');
 const input = document.querySelector('input');
+const search = document.getElementById('search');
+const liElements = document.querySelectorAll('li');
+
+const searchTask = (e) => {
+    const searchText = e.target.value.toLowerCase();
+    console.log(searchText);
+    let tasksSearch = toDoList;
+    tasksSearch = tasksSearch.filter(li => li.textContent.toLowerCase().includes(searchText));
+    console.log(tasksSearch);
+    ul.textContent = "";
+    tasksSearch.forEach(li => ul.appendChild(li))
+}
+
+
+
+search.addEventListener('input', searchTask);
 
 const removeTask = (e) => {
     e.target.parentNode.remove();
